6e6ca7d7fe597f410085dd639a22643c7cf73a5f
HazeXVIII <cyuan8171@gmail.com>
2024-06-27 19:51:53 -0700

    search function for api functional

--- app/components/Querylist.tsx
+++ app/components/Querylist.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Querylist = () => {
   return (
-    <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap">Querylist</div>
-  )
-}
+    <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link href="/test" className="relative w-full h-80">
+        <Image src="" alt="" fill sizes="25vw" />
+      </Link>
+    </div>
+  );
+};
 
-export default Querylist
+export default Querylist;
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,36 @@
-const SearchPage = () => {
-  return (
-    <div className="">SearchPage</div>
-  )
-}
+import Link from "next/link";
+import { Key } from "react";
 
-export default SearchPage
+export default async function SearchPage() {
+  const getCards = async () => {
+    const res = await fetch(
+      "https://api.pokemontcg.io/v2/cards?q=name:umbreon"
+    );
+    return res.json();
+  };
+
+  const data = await getCards();
+
+  return (
+    <div className="">
+      <ul className="grid grid-cols-4 gap-4 items-center">
+        {data.data.map(
+          (item: {
+            number: string;
+            set: any; 
+            images: any; 
+            name: string }, 
+            index: Key | null) => (
+              <li className="flex flex-center p-8" key={index}>
+                <Link href="/" className="relative">
+                  <img src={item.images.large} />
+                  <h1 className="font-bold text-lg">{item.name} #{item.number}</h1>
+                  {item.set.name}           
+                </Link>
+              </li>                
+          )
+        )}
+      </ul>
+    </div>
+  );
+}

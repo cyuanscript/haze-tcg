80d4ae91919ba7d1504346b9c100954c32ebf227
HazeXVIII <cyuan8171@gmail.com>
2024-12-22 14:01:30 -0800

    updated nextConfig

--- app/cards/[id]/page.tsx
+++ app/cards/[id]/page.tsx
@@ -1,9 +1,8 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
 
-interface getIdParams {
-    id?: string
-    params?: any
-    large?: any
+interface CardPageProps {
+  params: { id: string };
 }
 
 function getPrice(item: any) {
@@ -29,28 +28,41 @@ function getPrice(item: any) {
   return USD.format(thePrice);
 }
 
-export default async function cardPage(params: getIdParams) {
-    const id = params.params.id
-    const getCard = async () => {
-        const res = await fetch(
-          `https://api.pokemontcg.io/v2/cards/${id}`
-        );
-        return res.json();
-      };
+const CardPage = async ({ params }: CardPageProps) => {
+  const { id } = params;
 
-    const theCard = await getCard();
+  const res = await fetch(`https://api.pokemontcg.io/v2/cards/${id}`);
+  const theCard = await res.json();
 
   return (
     <div className="flex my-20 mx-40 relative">
       <div className="ml-40">
-        <img className="transition ease-in-out delay-150 hover:scale-105 hover:shadow-xl rounded-md shadow-md" src={theCard.data.images.large} width={350}></img>
-        <img className="w-36 top-full absolute z-10" src={theCard.data.set.images.logo} alt="set logo"></img>
+        <Image
+          className="transition ease-in-out delay-150 hover:scale-105 hover:shadow-xl rounded-md shadow-md"
+          src={theCard.data.images.large}
+          width={350}
+          height={350}
+          alt={theCard.data.name}
+        />
+        <Image
+          className="w-36 top-full absolute z-10"
+          src={theCard.data.set.images.logo}
+          width={144}
+          height={144}
+          alt="set logo"
+        />
       </div>
       <div className="text-4xl pt-3 px-6 font-bold">
-        <p>{theCard.data.name} - #{theCard.data.number}</p>
-        <Link className="text-xl font-normal px-1 pt-1" href={""}>{theCard.data.set.name}</Link>
+        <p>
+          {theCard.data.name} - #{theCard.data.number}
+        </p>
+        <Link className="text-xl font-normal px-1 pt-1" href="">
+          {theCard.data.set.name}
+        </Link>
         <p className="text-green-700">{getPrice(theCard.data)}</p>
       </div>
     </div>
   );
-}
+};
+
+export default CardPage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.pokemontcg.io',
+          port: '', // Leave empty if no specific port is required
+          pathname: '/**', // Allow all paths
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;

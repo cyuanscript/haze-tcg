8b4a612c96c8c84edaa6fc3e636779b91153f635
HazeXVIII <cyuan8171@gmail.com>
2024-12-23 03:28:58 -0800

    params fix

--- app/cards/[id]/page.tsx
+++ app/cards/[id]/page.tsx
@@ -2,29 +2,45 @@ import Link from "next/link";
 import Image from "next/image";
 import PriceChart from "../../components/PriceChart";
 
-// This is a server component. If you need hooks, you may need to designate specific components as client components.
-export default async function cardPage({ params }: { params: { id: string } }) {
-  const { id } = params;
+function getPrice(item: any) {
+  let thePrice;
+  let USD = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
-  async function getCardData(id: string) {
+  if (item.tcgplayer?.prices?.normal?.market) {
+    thePrice = item.tcgplayer.prices.normal.market;
+  } else if (item.tcgplayer?.prices?.holofoil?.market) {
+    thePrice = item.tcgplayer.prices.holofoil.market;
+  } else if (item.tcgplayer?.prices?.reverseHolofoil?.market) {
+    thePrice = item.tcgplayer.prices.reverseHolofoil.market;
+  } else if (item.tcgplayer?.prices?.unlimitedHolofoil?.market) {
+    thePrice = item.tcgplayer.prices.unlimitedHolofoil.market;
+  } else if (item.tcgplayer?.prices?.uncommon?.market) {
+    thePrice = item.tcgplayer.prices.uncommon.market;
+  } else {
+    return "N/A";
+  }
+  return USD.format(thePrice);
+}
+
+export default async function cardPage(params: any) {
+  const id = params.params.id;
+
+  const getCard = async () => {
     const res = await fetch(`https://api.pokemontcg.io/v2/cards/${id}`);
-    if (!res.ok) {
-      throw new Error('Failed to fetch card data');
-    }
     return res.json();
-  }
+  };
 
-  async function getPriceHistoryData(cardId: string) {
+  const getPriceHistory = async () => {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-    const res = await fetch(`${baseUrl}/api/priceHistory?cardId=${cardId}`);
-    if (!res.ok) {
-      throw new Error('Failed to fetch price history');
-    }
+    const res = await fetch(`${baseUrl}/api/priceHistory?cardId=${id}`);
     return res.json();
-  }
+  };
 
-  const theCard = await getCardData(id);
-  const priceHistory = await getPriceHistoryData(id);
+  const theCard = await getCard();
+  const priceHistory = await getPriceHistory();
 
   return (
     <div className="flex my-20 mx-40 relative">
@@ -58,27 +74,4 @@ export default async function cardPage({ params }: { params: { id: string } }) {
       </div>
     </div>
   );
-}
-
-function getPrice(item: any) {
-  let thePrice;
-  let USD = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-
-  if (item.tcgplayer?.prices?.normal?.market) {
-    thePrice = item.tcgplayer.prices.normal.market;
-  } else if (item.tcgplayer?.prices?.holofoil?.market) {
-    thePrice = item.tcgplayer.prices.holofoil.market;
-  } else if (item.tcgplayer?.prices?.reverseHolofoil?.market) {
-    thePrice = item.tcgplayer.prices.reverseHolofoil.market;
-  } else if (item.tcgplayer?.prices?.unlimitedHolofoil?.market) {
-    thePrice = item.tcgplayer.prices.unlimitedHolofoil.market;
-  } else if (item.tcgplayer?.prices?.uncommon?.market) {
-    thePrice = item.tcgplayer.prices.uncommon.market;
-  } else {
-    return "N/A";
-  }
-  return USD.format(thePrice);
 }
